6792000345f035bd12ccfda8a3e0799b9730dff1
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-09-29 10:33:12 -0500

    create main-view.jsx
    Create main-view.jsx, add MainView component, import useState, MovieCard, and MovieView.

---
+++ src/components/main-view/main-view.jsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([
+    { 
+        id: 1, 
+        title: 'The Avengers', 
+        image: 'https://www.pluggedin.com/wp-content/uploads/2019/12/the-avengers-review-image-1200x688.jpg',
+        director: 'Joss Whedon'
+    },
+    { 
+        id: 2, 
+        title: 'Pitch Perfect',
+        image: 'https://m.media-amazon.com/images/I/91bAKJ3lMVL._AC_UF894,1000_QL80_.jpg',
+        director: 'Jason Moore'
+    },
+    { 
+        id: 3, 
+        title: 'The Heat',
+        image: 'https://m.media-amazon.com/images/I/91IxCrm1VWL._AC_UF894,1000_QL80_.jpg',
+        direcotr: 'Paul Feig'
+    }
+  ]);
+
+  const [selectedMovie, setSelectedBook] = useState(null);
+
+  if (selectedMovie) {
+    return <MovieView movie={selectedMovie} />;
+  }
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  return (
+    <div>
+        {movies.map((movie) => (
+            <MovieCard                
+                key={movie.id}
+                movie={movie}
+                onMovieClick={(newSelectedMovie) => {
+                    setSelectedMovie(newSelectedMovie);
+                }}
+            />
+        ))}
+    </div>
+  );
+};
