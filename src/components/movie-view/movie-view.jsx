3cc20bd04e74569e76b87e67beb7d5b114f3f734
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-10-05 07:13:11 -0500

    update movie-view.jsx

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -2,15 +2,23 @@ export const MovieView = ({ movie, onBackClick }) => {
     return (
         <div>
             <div>
-                <img src={movie.image} />
+                <span>Title: </span>
+                <span>{movie.Title}</span>
             </div>
             <div>
-                <span>Title: </span>
-                <span>{movie.title}</span>
+                <span>Genre: </span>
+                <span>{movie.Genre.Name}</span>
+            </div>
+            <div>
+                <span>Description: </span>
+                <span>{movie.Description}</span>
             </div>
             <div>
                 <span>Director: </span>
-                <span>{movie.director}</span>
+                <span>{movie.Director.Name}</span>
+                <span>{movie.Director.Born}</span>
+                <span>{movie.Director.Died}</span>
+                <span>{movie.Director.Biography}</span>
             </div>
             <button onClick={onBackClick}>Back</button>
         </div>
