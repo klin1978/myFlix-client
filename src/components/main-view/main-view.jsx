f4aafb4711087e43fd2b83e05543081b0cbe23c1
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-10-09 09:13:25 -0500

    update main-view.jsx
    Add a logout button, apply form validation and authentication, store login details in localStorage, and import login and signup view.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,14 +1,27 @@
 import { useState, useEffect } from 'react';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
+import { LoginView } from '../login-view/login-view';
+import { SignupView } from '../signup-view/signup-view';
+
 
 export const MainView = () => {
+  const storedUser = JSON.parse(localStorage.getItem('user'));
+  const storedToken = localStorage.getItem('token');
   const [movies, setMovies] = useState([]);
-
   const [selectedMovie, setSelectedMovie] = useState(null);
+  const [user, setUser] = useState(null);
+  const [token, setToken] = useState(null);
 
   useEffect(() => {
-    fetch('https://my-films-9be1d0babd61.herokuapp.com/movies')
+    if (!token) {
+      return;
+    }
+
+    fetch('https://my-films-9be1d0babd61.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}`},
+    })
+    
       .then((response) => response.json())
       .then((data) => {
         const moviesFromApi = data.map((movie) => {
@@ -31,7 +44,17 @@ export const MainView = () => {
 
         setMovies(moviesFromApi);
       });
-  }, []);
+  }, [token]);
+
+  if (!user) {
+    return ( 
+      <>
+        <LoginView onLoggedIn={(user, token) => { setUser(user); setToken(token); }} />
+        or
+        <SignupView />
+      </>
+    );
+  }
 
   if (selectedMovie) {
     return (
@@ -40,7 +63,12 @@ export const MainView = () => {
   }
 
   if (movies.length === 0) {
-    return <div>The list is empty!</div>;
+    return (
+      <>
+      <button onClick={() => { setUser(null); setToken(null); localStorage.clear(); }}>Logout</button>
+      <div>The list is empty!</div>;
+      </>
+    );
   }
 
   return (
