09a6a689a92f1d0d402b8ce9881a438aa7e62f61
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-10-11 07:55:15 -0500

    update signup-view.jsx

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -16,7 +16,7 @@ export const SignupView = () => {
       Birthday: birthday
     };
 
-    fetch('SIGNUP_URL', {
+    fetch('https://my-films-9be1d0babd61.herokuapp.com/users', {
       method: 'POST',
       body: JSON.stringify(data),
       headers: {
