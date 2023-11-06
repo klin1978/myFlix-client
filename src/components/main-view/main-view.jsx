18e675de9cf3868d212a95ab68dd01e68d0bb855
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-11-06 08:37:10 -0600

    update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -10,7 +10,7 @@ import Col from 'react-bootstrap/Col';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 export const MainView = () => {
-  const storedUser = JSON.parse(localStorage.getItem('user'));
+  const storedUser = JSON.parse(localStorage.getItem('users'));
   const storedToken = localStorage.getItem('token');
   const [movies, setMovies] = useState([]);
   const [user, setUser] = useState(null);
@@ -29,7 +29,7 @@ export const MainView = () => {
       .then((data) => {
         const moviesFromApi = data.map((movie) => {
           return {
-            _id: movie.id,
+            id: movie._id,
             Title: movie.Title,
             Genre: {
               Name: movie.Genre.Name,
@@ -116,7 +116,7 @@ export const MainView = () => {
                 ) : (
                   <>
                     {movies.map((movie) => (
-                      <Col className='mb-4' key={movie.id} md={3}>
+                      <Col className='mb-4' key={movie._id} md={3}>
                         <MovieCard movie={movie} />
                       </Col>
                     ))}
