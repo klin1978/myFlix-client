3dd8e295aac7228ef0ba0118004b4a835857e35c
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-11-06 08:37:26 -0600

    update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -3,28 +3,27 @@ import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
+
 import './movie-card.scss';
 
 export const MovieCard = ({ movie }) => {
-    return (
-      <Card className='h-100'>
-        <Card.Img variant='top' src={movie.image} />
-        <Card.Body>
-          <Link to={`/movies/${encodeURIComponent(movie.id)}`} className='movie-link'>
-            <Card.Title>{movie.Title}</Card.Title>
-          </Link>
-          <Card.Text>{movie.Genre.Name}</Card.Text>
-          <Card.Text>{movie.Description}</Card.Text>
-          <Card.Text>{movie.Director.Name}</Card.Text>
-          <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
-            <Button variant='outline-dark' size='sm' className='open-btn'>Open</Button>
-          </Link>
-          <Link to=''>
-            <Button variant='outline-dark' size='sm' className='fav-btn'>Favorite</Button>
-          </Link>
-        </Card.Body>
-      </Card>
-    );
+
+  return (
+    <Card className='h-100'>
+      <Card.Img variant='top' src={movie.image} />
+      <Card.Body>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`} className='movie-link'>
+          <Card.Title>{movie.Title}</Card.Title>
+        </Link>
+        <Card.Text>{movie.Genre.Name}</Card.Text>
+        <Card.Text>{movie.Description}</Card.Text>
+        <Card.Text>{movie.Director.Name}</Card.Text>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button variant='outline-dark' size='sm' className='open-btn'>Open</Button>
+        </Link>
+      </Card.Body>
+    </Card>
+  );
 };
 
 MovieCard.propTypes = {
