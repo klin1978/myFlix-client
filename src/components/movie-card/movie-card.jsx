b2d5a8725d2b728bddd7be79fd9f8ce2683c9e64
klin1978 <124056493+klin1978@users.noreply.github.com>
2023-11-08 09:10:46 -0600

    update movie-card.jsx

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,26 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Button, Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-
 import './movie-card.scss';
 
-export const MovieCard = ({ movie }) => {
+export const MovieCard = ({ movie, setUser, user, token }) => {
+
+  const [isFavorite, setFavorite] = useState(user.FavoriteMovies.includes(movie.id));
+
+  const addFavoriteMovie = () => {
+    fetch(`https://my-films-9be1d0babd61.herokuapp.com/users/${user.Username}/movies/${movie.id}`, {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${token}` }
+    }
+    ).then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert('Failed to add movie')
+      }
+    }).then((data) => {
+      if (data) {
+        localStorage.setItem('user', JSON.stringify(data));
+        setUser(data);
+        setFavorite(true);
+        alert('Movie added!');
+      }
+    }).catch((error) => {
+      alert(error);
+    });
+  };
+
+  const removeFavoriteMovie = () => {
+    fetch(`https://my-films-9be1d0babd61.herokuapp.com/users/${user.Username}/movies/${movie.id}`, {
+      method: 'DELETE', 
+      headers: { Authorization: `Bearer ${token}` }
+    }).then((response) => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        alert('Failed to remove favorite movie');
+      }
+    }).then((data) => {
+        localStorage.setItem('user', JSON.stringify(data));
+        setUser(data);
+        setFavorite(false);
+        alert('Favorite movie removed');
+    }).catch((error) => {
+      alert(error);
+    });
+  };
 
   return (
     <Card className='h-100'>
       <Card.Img variant='top' src={movie.image} />
       <Card.Body>
         <Link to={`/movies/${encodeURIComponent(movie.id)}`} className='movie-link'>
-          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Title className='text-center mb-1'><h3>{movie.Title}</h3></Card.Title>
         </Link>
-        <Card.Text>{movie.Genre.Name}</Card.Text>
+        <Card.Text className='text-center'>{movie.Genre.Name}</Card.Text>
         <Card.Text>{movie.Description}</Card.Text>
-        <Card.Text>{movie.Director.Name}</Card.Text>
         <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
           <Button variant='outline-dark' size='sm' className='open-btn'>Open</Button>
         </Link>
+        {isFavorite ? (
+          <Button variant='outline-dark' size='sm' onClick={removeFavoriteMovie}>Remove Favorite</Button>
+        ) : (
+          <Button variant='outline-dark' size='sm' onClick={addFavoriteMovie}>Add to Favorites</Button>
+        )}
       </Card.Body>
     </Card>
   );
